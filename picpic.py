501790502e1150a15c972e05cd87930fb74c7028
will7101 <will7101c@gmail.com>
2017-07-21 22:02:48 +0800

    v1.1

--- picpic.py
+++ picpic.py
@@ -2,42 +2,65 @@
 # -*-coding=utf-8-*-
 
 from PIL import Image
+import getopt
 import sys
+import os
+
+
+def usage():
+    print("usage: %s [-s size] [-a alpha] <file name>" % sys.argv[0])
 
 
 def main():
-    # infile = sys.argv[1]
-    infile = "huaji.png"
     try:
+        opts, args = getopt.getopt(sys.argv[1:], "s:a:")
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+
+    try:
+        infile = args[0]
         im = Image.open(infile)
         print(im.size, im.mode)
-    except IOError:
-        print("Fail: Cannot open file")
-        sys.exit(0)
+    except IndexError as err:
+        print(err)
+        usage()
+        sys.exit(-1)
+    except IOError as err:
+        print(err)
+        print("Error: cannot open file")
+        sys.exit(-1)
 
     sz = 64
+    alpha = 0.5
+
+    for o, a in opts:
+        if o == "-s":
+            sz = int(a)
+        elif o == "-a":
+            alpha = float(a)
+
+    if not (0 < sz <= 256 and 0.0 <= alpha <= 1.0):
+        print("Error: out of range")
+        sys.exit(-1)
+
     print("resizing...")
     im = im.resize((sz, sz))
 
-    if im.mode != "RGB":
-        im = im.convert("RGB")
+    layer0 = im.resize((sz * sz, sz * sz), Image.NEAREST)
+    layer1 = Image.new(im.mode, (sz * sz, sz * sz))
 
-    bands = im.split()
-    big_bands = []
-    for i in [0, 1, 2]:
-        tmp = bands[i].point(lambda x: (x - 128) // 2 + 128)
-        lookup = [tmp.point(lambda x: x + d) for d in range(-64, 64)]
+    for x in range(sz):
+        for y in range(sz):
+            layer1.paste(im, (x * sz, y * sz))
 
-        tmp = tmp.load()
-        big_tmp = Image.new("L", (sz * sz, sz * sz))
-        for x in range(64):
-            for y in range(64):
-                big_tmp.paste(lookup[tmp[x, y] - 64], (x * 64, y * 64))
+    print("merging...")
+    output = Image.blend(layer0, layer1, alpha)
 
-        big_bands.append(big_tmp)
-
-    big_im = Image.merge("RGB", tuple(big_bands))
-    big_im.save("output.png", "PNG")
+    outfile = os.path.splitext(infile)[0] + "_picpic.png"
+    output.save(outfile, "PNG")
+    print("done.")
 
 
 if __name__ == "__main__":
